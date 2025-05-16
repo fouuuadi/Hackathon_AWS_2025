b7d782223dc2f1fd5dec3a08c4b6ad50b4ea4fa8
Jeck0v <arnaud.fischer75@gmail.com>
2025-05-17 01:31:29 +0200

    fix dynamodb

--- app/repositories/card_repository.py
+++ app/repositories/card_repository.py
@@ -1,15 +1,14 @@
+import os
 from app.extensions import dynamodb
 from uuid import uuid4
 
-TABLE_NAME = "cards"
+TABLE_NAME = os.getenv("DYNAMODB_TABLE_CARDS", "cards")
 table = dynamodb.Table(TABLE_NAME)
 
 
 def get_card(card_id: str) -> dict:
     """
     Récupère une carte par son ID.
-    - card_id : chaîne de caractères de l'ObjectId Mongo.
-    - Retourne un dict standardisé ou None si introuvable.
     """
     try:
         response = table.get_item(Key={"id": card_id})
@@ -25,8 +24,6 @@ def get_card(card_id: str) -> dict:
 def create_card(data: dict) -> dict:
     """
     Crée une nouvelle carte.
-    - data : dict contenant les clés url, text, img, note, tags, resume_information.
-    - Insère le document en base et retourne le dict créé avec l'ID.
     """
     try:
         card_id = str(uuid4())
@@ -36,4 +33,3 @@ def create_card(data: dict) -> dict:
     except Exception as e:
         print(f"Erreur lors de la création de la carte : {e}")
         return None
-
--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,26 +1,45 @@
-from app.extensions import mongo
+import boto3
 from werkzeug.security import generate_password_hash, check_password_hash
+from botocore.exceptions import ClientError
+import os
+from dotenv import load_dotenv
 
-USERS = mongo.db.users
+load_dotenv()
+
+# Chargement du nom de la table depuis .env
+TABLE_NAME = os.getenv("DYNAMODB_TABLE_USERS", "users")
+dynamodb = boto3.resource('dynamodb', region_name='eu-west-3')  # adapte la région si besoin
+USERS = dynamodb.Table(TABLE_NAME)
 
 def get_by_username(username: str):
-    doc = USERS.find_one({"username": username})
-    if not doc:
+    try:
+        response = USERS.get_item(Key={"username": username})
+    except ClientError as e:
+        print("Erreur DynamoDB:", e.response['Error']['Message'])
         return None
+
+    if "Item" not in response:
+        return None
+
+    item = response["Item"]
     return {
-        "id":       str(doc["_id"]),
-        "username": doc["username"],
-        "password": doc["password"]
+        "username": item["username"],
+        "password": item["password"]
     }
 
 def create_user(username: str, password: str):
     pw_hash = generate_password_hash(password)
-    res     = USERS.insert_one({
-        "username": username,
-        "password": pw_hash
-    })
+
+    try:
+        USERS.put_item(Item={
+            "username": username,
+            "password": pw_hash
+        })
+    except ClientError as e:
+        print("Erreur DynamoDB:", e.response['Error']['Message'])
+        return None
+
     return {
-        "id":       str(res.inserted_id),
         "username": username,
         "password": pw_hash
     }
