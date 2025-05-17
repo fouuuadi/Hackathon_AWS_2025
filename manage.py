0cd013fd504cd78f64856bff38502db1f8f0dcb8
Jeck0v <arnaud.fischer75@gmail.com>
2025-05-17 04:45:41 +0200

    mental break down 3

--- manage.py
+++ manage.py
@@ -1,24 +1,10 @@
-from app import create_app                    # Importe la factory pour créer l'instance Flask
-from flask_migrate import Migrate               # Extension pour gérer les migrations Alembic
-from app.extensions import dynamodb                   # Instance SQLAlchemy déclarée dans extensions
+from flask import Flask
 
-# Création de l'application à partir de la factory
-app = create_app()
-# Liaison de Flask-Migrate avec l'app et la base de données
-migrate = Migrate(app, dynamodb)
+app = Flask(__name__)
 
-# Route de test basique (Hello World) pour vérifier que l'app tourne
-@app.route('/', methods=['GET'])
+@app.route('/')
 def hello_world():
     return "Hello, World!"
 
-# Point d'entrée pour exécution directe via `python manage.py`
 if __name__ == '__main__':
-    # recrée l'app pour s'assurer que la factory est utilisée
-    app = create_app()
-    # lance le serveur de développement sur 0.0.0.0:5005 avec debug activé
-    app.run(host='0.0.0.0', port=5005, debug=True)
-
-# Note :
-# - FLASK_APP=manage.py permet à la CLI `flask` de découvrir `app` pour les commandes.
-# - La partie `if __name__ == '__main__'` est utilisée uniquement pour le démarrage direct.
+    app.run(host='0.0.0.0', port=5005)
